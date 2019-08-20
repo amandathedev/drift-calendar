17f27674decd99af4bbb7105cd132a4bc33a60ea
Amanda Treutler <amandathedev@gmail.com>
2019-08-20 15:50:37 -0500

    HTML elements

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,3 +4,6 @@ const userUrl = `${rootUrl}/users`
 const eventUrl = `${rootUrl}/events`
 const usereventsUrl = `${rootUrl}/userevents`
 
+// HTML Components
+const calendarDiv = document.getElementById('calendar')
+const monthDiv = document.getElementById('month')
