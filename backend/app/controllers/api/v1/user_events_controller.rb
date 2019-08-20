d698c65df5f62db1117cba5593489bd0d59a2de0
Adrian M Popan <adrian.popan@gmail.com>
2019-08-20 15:41:39 -0500

    api show configuration

--- backend/app/controllers/api/v1/user_events_controller.rb
+++ backend/app/controllers/api/v1/user_events_controller.rb
@@ -3,7 +3,7 @@ module Api
     class UserEventsController < ActionController::Base
         def index
           user_events = UserEvent.all
-          render json: user_events, exclude: [:timestamps]
+          render json: user_events, exclude: [:created_at, :updated_at]
         end
       end
     end
--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -3,8 +3,18 @@ module Api
     class UsersController < ActionController::Base
         def index
           users = User.all
-          render json: users, exclude: [:timestamps]
+          render json: users, exclude: [:created_at, :updated_at]
         end
+
+        def show
+          user = User.find_by(id: params[:id])
+          if user
+            render json: user, include: [:events], exclude: [:created_at, :updated_at]
+          else 
+            render json: { message: 'No user found with that id' }
+          end
+        end
+
       end
     end
   end  
