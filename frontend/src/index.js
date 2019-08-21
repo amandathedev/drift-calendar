aa1017f371d1973281b8cc2c9dcd5d109704e0e1
Amanda Treutler <amandathedev@gmail.com>
2019-08-21 10:43:43 -0500

    Variable style

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,6 +8,68 @@ const usereventsUrl = `${rootUrl}/userevents`
 const calendarDiv = document.getElementById('calendar')
 const monthDiv = document.getElementById('month')
 const h1 = document.getElementById('title')
-const daySpans = document.getElementsByClassName('day')
+let daySpans = document.getElementsByClassName('day')
+let dayBoxes = document.getElementById('dayBoxes')
+let monthTitle = document.getElementById('monthTitle')
+let yearTitle = document.getElementById('yearTitle')
+
+// Current day
+let currentDay = () => {
+  let date = new Date()
+  let today = date.getDate()
+  console.log(today);
+  document.getElementById(today).className += " current"
+  // Title
+  // monthTitle = 
+}
+
+// Read
+const fetchEvents = () => {
+  fetch(eventUrl)
+  .then(resp => resp.json())
+  .then(events => {
+    events.forEach(event => {renderEvent(event)})
+  })
+}
+
+const createDays = () => {
+  for (let i = 0; i < 35; i++) {
+    let dayBox = document.createElement('span')
+    dayBoxes.append(dayBox)
+    dayBox.className = "day"
+    dayBox.innerText = i + 1
+    dayBox.id = i + 1
+  }
+  currentDay()
+}
+
+// Change display for users
+const displayUser = () => {
+  let navbarPics = document.getElementById('avatar-bar')
+  navbarPics.addEventListener('click', () => {
+    let selectedUser = event.target.parentNode.parentNode
+    if (selectedUser.id == 1) {
+      document.body.style.backgroundColor = "red"
+    }
+    if (selectedUser )
+      // Fetch individual user's events
+      // Iterate and display event indicator
+      // Store in a global variable
+  })
+}
+
+// Create
+
+
+// Update
+
+
+// Destroy
+
 
 // Click listeners
+
+
+// Function calls
+createDays()
+displayUser()
