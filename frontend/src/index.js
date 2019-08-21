14c3a0b57358b18ca3461c6e247bcd167ee0bb1d
Amanda Treutler <amandathedev@gmail.com>
2019-08-21 08:28:12 -0500

    Global variables

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,3 +7,7 @@ const usereventsUrl = `${rootUrl}/userevents`
 // HTML Components
 const calendarDiv = document.getElementById('calendar')
 const monthDiv = document.getElementById('month')
+const h1 = document.getElementById('title')
+const daySpans = document.getElementsByClassName('day')
+
+// Click listeners
